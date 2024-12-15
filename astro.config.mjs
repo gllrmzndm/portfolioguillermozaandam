409e019e242d7b53e70b4deaed51dd495956c562
gllrmzndm <guillermozaandam@hotmail.com>
2024-12-15 21:24:50 +0100

    Serviceworker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import serviceWorker from "astrojs-service-worker";
+
+
 // https://astro.build/config
 export default defineConfig({
 
     site: 'https://gllrmzndm.github.io',
     base: '',
-    integrations: [tailwind()],
+    integrations: [tailwind(), serviceWorker()], 
 });
